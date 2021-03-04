49c428f1b2f64fdd0de0eff0ec6eb75ce5dabfe7
Ahmad Ferdaus Abd Razak <ahmad.ferdaus.abd.razak@ni.com>
2021-03-04 15:48:07 +0800

    Updated code to return instead of exit on exceptions.

--- src/playsongs/playsongs.py
+++ src/playsongs/playsongs.py
@@ -7,11 +7,13 @@ from playsound import playsound
 
 def PlaySongs(path, repeat = 0, shuffle = False):
 
-    # Create the list of files from a directory of songs.
-    file_list = os.listdir(path)
-
-    # Filter list to only include MP# files.
-    song_list = [i for i in file_list if i.endswith('.mp3')]
+    try:
+        # Create the list of files from a directory of songs.
+        file_list = os.listdir(path)
+        # Filter list to only include MP# files.
+        song_list = [i for i in file_list if i.endswith('.mp3')]
+    except Exception as e:
+        return str(e)
 
     # Initialize repeat counter.
     counter = 0
@@ -24,16 +26,18 @@ def PlaySongs(path, repeat = 0, shuffle = False):
             random.shuffle(song_list)
 
         try:
-
             # Play songs.
             for song in song_list:
                 print('Playing: {}/{}...'.format(path, song))
                 playsound('{}/{}'.format(path, song))
         
         except KeyboardInterrupt:
-
-            print('\nStopped')
-            sys.exit(0)
+            return 'Stopped'
+        
+        except Exception as e:
+            return str(e)
         
         # Increment counter.
         counter += 1
+
+    return 'Done'
